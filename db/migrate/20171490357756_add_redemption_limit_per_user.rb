d77878af6337cc8f73a07de27ff98fb4c86448e4
suslov <suslov@qburst.com>
2017-03-24 18:25:20 +0530

    implemented redemption limit per user

--- db/migrate/20171490357756_add_redemption_limit_per_user.rb
+++ db/migrate/20171490357756_add_redemption_limit_per_user.rb
@@ -1,4 +1,4 @@
-class UpdateCouponRedemptions < ActiveRecord::Migration
+class AddRedemptionLimitPerUser < ActiveRecord::Migration
   def change
     add_column :coupon_redemptions, :redemption_limit_per_user, :integer, default: 1, null: false
   end
