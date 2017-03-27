21798db3f4128e6dd7d19420963e8b1f4db49581
suslov <suslov@qburst.com>
2017-03-27 17:02:38 +0530

    redemption limit per user logic changes

--- lib/coupons/finders/first_available.rb
+++ lib/coupons/finders/first_available.rb
@@ -1,8 +1,15 @@
 module Coupons
   module Finders
     FirstAvailable = proc do |code, options = {}|
-      coupons = Models::Coupon.where(code: code).all
-      coupons.find(&:redeemable?)
+      coupons = Models::Coupon.where(code: code).all.select(&:redeemable?)
+      if coupons.present?
+        coupons.each do |coupon|
+          if coupon.has_available_user_redemptions?(options[:user_id])
+            return coupon
+          end
+        end
+      end
+      return nil
     end
   end
 end
