1c93179745aaaa6f9df292dd22ce1af97745e729
suslov <suslov@qburst.com>
2017-03-30 17:26:53 +0530

    amount type changed

--- db/migrate/20170330357756_change_amount_type.rb
+++ db/migrate/20170330357756_change_amount_type.rb
@@ -1,6 +1,6 @@
 class ChangeAmountType < ActiveRecord::Migration
   def up
-    change_column :coupons, :amount, :decimal, null: false, default: 0
+    change_column :coupons, :amount, :decimal, :precision => 10, :scale => 2
   end
 
   def down
