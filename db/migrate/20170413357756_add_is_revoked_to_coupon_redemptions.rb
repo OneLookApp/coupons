62c98aaf1bf4745f3337abf31996c00f7a042ba3
suslovbabu <suslov@qburst.com>
2017-04-13 01:22:26 -0400

    redemption related changes

--- db/migrate/20170413357756_add_is_revoked_to_coupon_redemptions.rb
+++ db/migrate/20170413357756_add_is_revoked_to_coupon_redemptions.rb
@@ -1,4 +1,4 @@
-class AddRedemptionLimitPerUser < ActiveRecord::Migration
+class AddIsRevokedToCouponRedemptions < ActiveRecord::Migration
   def change
     add_column :coupon_redemptions, :is_revoked, :boolean, default: 0, null: false
   end
